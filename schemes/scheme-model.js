b7f78f5d25580327c16de1da4c3dc091568cec7b
Daniel Cruz <d.cruz.489@hotmail.com>
2020-02-04 21:28:47 -0700

    helpers complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,9 +11,42 @@ function findById(id) {
     .first();
 }
 
+function findSteps(schemeId) {
+    return db('schemes')
+    .join('steps', 'steps.scheme_id', '=', 'schemes.id')
+    .where('schemes.id', schemeId)
+    .select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .orderBy('steps.step_number');
+}
+
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme)
+    .then(([id]) => {
+        return findById(id);
+    });
+}
+
+function update(changes, id) {
+    return db('schemes')
+    .where('id', id)
+    .update(changes);
+}
+
+function remove(id) {
+    return db('schemes')
+    .where('id', id)
+    .del()
+    .then(deleted => {
+        return deleted;
+    })
+}
 
 module.exports = {
     find,
     findById,
-  
+    findSteps,
+    add,
+    update,
+    remove,
 }
